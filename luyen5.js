0c56a0a72792f1ee8b01ef0e8ab7238e158b95e2
PHAMTIENHUNG772006 <hungpham772006@gmail.com>
2025-03-27 23:53:11 +0700

    Add files via upload

--- luyen5.js
+++ luyen5.js
@@ -5,48 +5,6 @@ const pendingTasks = document.getElementById("pendingTasks");
 const inProgressTasks = document.getElementById("inProgressTasks");
 const doneTasks = document.getElementById("doneTasks");
 
-let tasks = JSON.stringify(localStorage.getItem("task")) || [];
-
-function renderTasks() {
-    
-    tasks.forEach((task, index) => {
-        let taskDiv = document.createElement("div");
-        taskDiv.classList.add("task");
-        taskDiv.innerHTML = `
-            <p>${task}</p>
-            <button onclick="deleteTask(${index})">Xóa</button>
-        `;
-        pendingTasks.appendChild(taskDiv);
-    });
-}
-
-addButton.addEventListener("click", function(){
-    let input = taskInput.value.trim();
-    if(input === ""){
-        alert(" Không được đẻ trống dữ liệu");
-        return;
-    }
-    let task = {
-        input,
-    }
-    tasks.push(task);
-    localStorage.setItem("task", JSON.stringify(tasks));
-    document.createElement("div");
-
-    renderTasks();
-})
-
-renderTasks();
-// Lấy các phần tử cần thiết
-
-
-// Lấy các phần tử cần thiết
-const taskInput = document.getElementById("taskName");
-const addButton = document.querySelector(".task-input button");
-const pendingTasks = document.getElementById("pendingTasks");
-const inProgressTasks = document.getElementById("inProgressTasks");
-const doneTasks = document.getElementById("doneTasks");
-
 // Tải công việc từ localStorage
 function loadTasks() {
     pendingTasks.innerHTML = localStorage.getItem("pending") || "";
@@ -98,6 +56,4 @@ addButton.addEventListener("click", () => {
     taskInput.value = "";
 });
 
-// Tải công việc khi trang mở
-window.onload = loadTasks;
-
+loadTasks();
